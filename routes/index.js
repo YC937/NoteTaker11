6a310997f0727cae093ff517f809a90d085b1d75
unknown <ymingc.12@gmail.com>
2023-08-20 23:24:47 -0400

    Added routes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,8 @@
 // Import the router
-
 const router = require('express').Router();
 
 // Import the notes module
 const notesRouter = require('./notes');
-
 router.use('./notes', notesRouter);
 
 // Export router
---
+++ routes/notes.js
@@ -0,0 +1,46 @@
+const notes = require('express').Router();
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const uuid = require('../helpers/uuid');
+const db = require("../db/notes.json");
+const fs = require("fs");
+let notesData = db;
+// Use get route for retrieving all the notes
+notes.get('/notes', (req, res) => {
+  console.info(`${req.method} request received for notes`);
+  readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// Post route for another note
+notes.post('/notes', (req, res) => {
+  console.info(`${req.method} request received to add a note`);
+  console.log(req.body);
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNotes = {
+      title,
+      text,
+      id: uuid(),
+    };
+
+    readAndAppend(newNotes, './db/notes.json');
+    res.json(`Notes added successfully 🚀`);
+  } else {
+    res.error('Error in adding notes');
+  }
+});
+
+notes.delete("/notes/:id", (req, res) => {
+  const Delete = req.params.id;
+  notesData = notesData.filter((note) => note.id !== Delete);
+
+  const notesString = JSON.stringify(notesData);
+
+  fs.writeFile(`./db/notes.json`, notesString, (err) => {
+    err ? console.log(err) : console.log(`New note for has been saved!`);
+    res.send();
+  });
+});
+
+
+module.exports = notes;
